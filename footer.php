9bfaad7bb36ed28ec7fc126ea0c80b570e50a72a
Oritse J. Uku <oritseuku@Oritse-J-Ukus-MacBook-Pro.local>
2016-07-31 21:37:29 -0500

    first commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Roman_Cavalry
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'roman-cavalry' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'roman-cavalry' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'roman-cavalry' ), 'roman-cavalry', '<a href="http://oritseuku.com" rel="designer">Oritse Justin Uku</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
