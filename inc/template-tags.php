2c1c5939df7d12b89d9ed8d57b88d4f5555c5f10
Oritse J. Uku <oritse@oritseuku.com>
2016-08-11 14:13:56 -0500

    styled the post meta

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,16 +25,31 @@ function roman_cavalry_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'roman-cavalry' ),
+		esc_html_x( ' %s', 'post date', 'roman-cavalry' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
 	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'roman-cavalry' ),
+		esc_html_x( ' %s', 'post author', 'roman-cavalry' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="posted-on"><i class="fa fa-calendar" aria-hidden="true"></i> ' . $posted_on . '</span><span class="byline"><i class="fa fa-user" aria-hidden="true"></i>' . $byline . '</span>'; // WPCS: XSS OK.
+    
+    if ( 'post' === get_post_type() ) {
+		/* translators: used between list items, there is a space after the comma */
+		$categories_list = get_the_category_list( esc_html__( ', ', 'roman-cavalry' ) );
+		if ( $categories_list && roman_cavalry_categorized_blog() ) {
+			printf( '<span class="cat-links"><i class="fa fa-folder-open" aria-hidden="true"></i> ' . esc_html__( ' %1$s', 'roman-cavalry' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+		}
+	}
+    
+    if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		echo '<span class="comments-link"><i class="fa fa-comments" aria-hidden="true"></i> ';
+		/* translators: %s: post title */
+		comments_popup_link( sprintf( wp_kses( __( ' Leave a Comment<span class="screen-reader-text"> on %s</span>', 'roman-cavalry' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
+		echo '</span>';
+	}
 
 }
 endif;
@@ -46,24 +61,12 @@ if ( ! function_exists( 'roman_cavalry_entry_footer' ) ) :
 function roman_cavalry_entry_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'roman-cavalry' ) );
-		if ( $categories_list && roman_cavalry_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'roman-cavalry' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
 
 		/* translators: used between list items, there is a space after the comma */
 		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'roman-cavalry' ) );
 		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'roman-cavalry' ) . '</span>', $tags_list ); // WPCS: XSS OK.
+			printf( '<span class="tags-links"><i class="fa fa-tags" aria-hidden="true"></i> ' . esc_html__( ' %1$s', 'roman-cavalry' ) . '</span>', $tags_list ); // WPCS: XSS OK.
 		}
-	}
-
-	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
-		echo '<span class="comments-link">';
-		/* translators: %s: post title */
-		comments_popup_link( sprintf( wp_kses( __( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'roman-cavalry' ), array( 'span' => array( 'class' => array() ) ) ), get_the_title() ) );
-		echo '</span>';
 	}
 
 	edit_post_link(
